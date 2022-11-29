7bf2191f75c9cc2ed8cb8dd9e40572c1c543c812
utkarsh <utkarshnanda9@gmail.com>
2022-11-30 00:01:07 +0530

    cors update 3

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ const __dirname1 = path.resolve();
 const app = express()
 const port = process.env.PORT || 5000
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+app.use(cors());
 
 app.use(express.json())
 app.use(user_router)
@@ -56,19 +56,19 @@ app.use(community_router)
 app.use("/chat", chat_router)
 app.use("/message", message_router)
 
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-app.use(cors());
+// app.use(cors(corsOptions));
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Max-Age", "1800");
+//   res.setHeader("Access-Control-Allow-Headers", "content-type");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
+
 
 const server = app.listen(port, () => {
     console.log('Server is up on port ' + port)
